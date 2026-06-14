3dc194d0bc17864d66411024133c1e9cbe0d62ed
pamvoberynda <prepotishnyi@gmail.com>
2026-06-14 15:33:01 +0300

    minor issues r solved

--- syllabizer.py
+++ syllabizer.py
@@ -1,12 +1,11 @@
 # creates digitilized view of letters
 gluhi = ("ц", "с", "к", "ф", "п", "т", "х", "ш", "ч", "щ") #0
 dzvinki = ("д", "г", "ґ", "з", "б", "ж") #1
-sonorni = ("й", "н", "ь", "м", "л", "р", "в") #2
-#3 - double sonorni
+sonorni = ("й", "н", "м", "л", "р", "в") #2
+double_soft = ("ь",) #double sonorni or soft sign
 golosni = ("а", "я", "о", "е", "є", "у", "ю", "и", "і", "ї") #4
 #5 - space or unknown character etc
-letters = gluhi + dzvinki + sonorni
-
+letters = gluhi + dzvinki + sonorni + golosni + double_soft
 
 def digitilize_word(word: str) -> str:
     dig_word = str()
@@ -26,6 +25,8 @@ def digitilize_word(word: str) -> str:
             dig_word += "2"
         elif letter in golosni:
             dig_word += "4"
+        elif letter in double_soft:
+            dig_word += "3"
         else:
             dig_word += "5"
     return dig_word
@@ -87,7 +88,10 @@ def review_raw_syllables(raw_syllables: list) -> list:
                             prev_syll.append(cur_symb)
                             curr_syll.pop(0) 
                             break
-                
+            if len(curr_syll) == 1 and curr_syll[0] not in (4,5): # 1 | 04 -> 104
+                curr_syll.extend(next_syll)
+                i+=1
+
             i+=1
             done_syllables.append(curr_syll)
             
@@ -116,11 +120,19 @@ def separate_syllables(word: str, separator="-"):
     add_sep_idx = int()
     for syllable in correlated_syllables:
         output += syllable
-        if add_sep_idx < lenght-1 and output[-1] != " ":
+        if add_sep_idx < lenght-1 and output[-1] in letters:
             output+= separator
         add_sep_idx+=1
     return output
 
 
-worde = "що ж ти фраєре здав назад, не по масті я тобі. ти дивись в мої очі. кидай полошитись по долі"
+worde = """Кольщик, наколи мені куполи,
+Поруч — чудотворний хрест з іконами,
+Щоб там дзвонили дзвони
+З переливами та передзвонами.
+ 
+Наколи мені будиночок біля струмка,
+Нехай тече на волі тонкою цівкою.
+Щоб від нього кравець-суддя
+Не відгородив мене ґратами."""
 print(separate_syllables(worde))
